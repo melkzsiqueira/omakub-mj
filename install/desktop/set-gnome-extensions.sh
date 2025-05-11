9a298b82a634f849ed2111cd98a1a909f7c29543
Melkz Siqueira <melkz.siqueira@gmail.com>
2025-05-11 01:54:18 -0300

    chore: corrige script do space bar

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -17,6 +17,7 @@ gext install AlphabeticalAppGrid@stuarthayhurst
 sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
 sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+sudo mkdir ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/
 sudo mkdir ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas
 sudo cp /usr/share/glib-2.0/schemas/org.gnome.shell.extensions.space-bar.gschema.xml ~/.local/share/gnome-shell/extensions/space-bar@luchrioh/schemas
 sudo cp ~/.local/share/gnome-shell/extensions/tophat@fflewddur.github.io/schemas/org.gnome.shell.extensions.tophat.gschema.xml /usr/share/glib-2.0/schemas/
